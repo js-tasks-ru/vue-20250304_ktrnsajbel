22ddc960bdbcb8a8dddc928cb9bbb056dd25338d
Ekaterina Sai <ktrnsajbel@gmail.com>
2025-03-09 18:54:59 +0500

    display current date

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,23 @@
 import { defineComponent, createApp } from 'vue'
+
+const App = defineComponent({
+  name: 'App',
+  setup () {
+    const date = new Date;
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    const formattedDate = date.toLocaleDateString('en-EN', options);
+
+    return { formattedDate };
+  },
+
+  template: `<div>
+    <p>Сегодня {{ formattedDate }}</p>
+  </div>`,
+});
+
+const app = createApp(App);
+app.mount('#app');
